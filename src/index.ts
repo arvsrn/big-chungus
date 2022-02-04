bd0981d17cd939b41fa2fc5d5e775b3442d7b926
duckie451 <96787824+duckie451@users.noreply.github.com>
2022-02-04 21:11:33 +0530

    Basic command handler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,44 @@
-import { Client } from "discord.js";
+import { Database, Guild } from "./database";
+import { log } from "./logging";
 import { token } from "./config.json";
+import { CommandHandler } from "./handler";
+
+import { Client, Intents } from "discord.js";
 
 const client = new Client({
-    intents: []
+    intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]
 });
+
+const database = new Database();
+const commandHandler = new CommandHandler();
+
+commandHandler.registerCommand("ping", async (message, database) => {
+    await message.reply("Large Chungus says hello from TypeScript!");
+});
+
+client.once("ready", async (client) => {
+    log(`b{Logged in as ${client.user.username}.}`);
+
+    for (const guild of client.guilds.cache.values()) {
+        database.insertGuild(guild.id, {
+            moderators: []
+        }).then(() => {});
+    }
+})
+
+/* Text command handler */
+client.on("messageCreate", async message => {
+    if (message.content.startsWith("!")) {
+        /* Weird but working code to extract command name */
+        let commandName = message.content
+            .toLowerCase()
+            .split(" ")[0]
+            .slice(1, message.content.length);
+
+        let executeFunction = commandHandler.retrieveCommand(commandName);
+        if (executeFunction) await executeFunction(message, database);
+    }
+})
 
 /* Go to src/config.json and put your token there. */
 client.login(token);
