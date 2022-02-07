c596dbe0805f2383724275ba170a5fb70857a29e
duckie451 <96787824+duckie451@users.noreply.github.com>
2022-02-07 10:07:04 +0530

    Modified `Guild` and added functions to database

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,20 @@
 import Keyv from "keyv";
 
+/* Stores all the data that needs to be cached when anti-raid mode is turned on. */
+export interface RaidCache {
+    bannedUsers: string[],
+}
+
 /* Represents a discord guild and is used in the database */
 export interface Guild {
     moderators: string[],
+
+    antiRaid: boolean,
+    unsafeMode: boolean,
+
+    raidCache: RaidCache,
+    banCache: string[],
+    eventRateCache: Map<string, number>
 }
 
 export class Database {
@@ -10,6 +22,19 @@ export class Database {
 
     constructor() {
         this.inner = new Keyv({});
+    }
+
+    async defaultGuild(id: string) { 
+        await this.insertGuild(id, {
+            moderators: [],
+
+            antiRaid: false,
+            unsafeMode: false,
+
+            raidCache: { bannedUsers: [] },
+            banCache: [],
+            eventRateCache: new Map()
+        });
     }
 
     async insertGuild(id: string, guild: Guild) {
